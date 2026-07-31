b4aea5f8984516ff8f3745ff9b1cd20ea5fb2934
jenn530 <jenniferchidera844@gmail.com>
2026-07-31 01:24:46 +0000

    Created homepage for TAVIA DAZZLE STORE

---
+++ frontend/src/main.ts
@@ -0,0 +1,11 @@
+
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <header>
+      <img src="/IMG-20260731-WA0000.jpg" alt="TAVIA DAZZLE STORE Logo" width="150">
+          <h1>TAVIA DAZZLE STORE</h1>
+              <p>Luxury Jewellery, Watches & Accessories</p>
+                  <button>Shop Now</button>
+                    </header>
+                    `
